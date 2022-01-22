1f26e86a4bddbc0a15aa43ccb5ab680b1dd31463
Ralf Jovenal <ralf.jovenal@coreproc.com>
2022-01-22 20:20:07 +0800

    use secure helper

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <style>
         body {
             font-family: 'Nunito', sans-serif;
